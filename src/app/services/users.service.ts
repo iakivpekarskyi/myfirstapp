89470827c80044683a5644d9d8a294fb9ccbe1e3
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2024-03-12 20:47:55 +0100

    feat: add users service (CRUD)

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,1 +1,23 @@
 import { Injectable } from '@angular/core';
+import { User } from '../types/users.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  private users: User[] = [];
+
+  constructor() {}
+
+  getUsers(): User[] {
+    return this.users;
+  }
+
+  addUser(user: User): void {
+    this.users.concat(user);
+  }
+
+  deleteUser(id: number): void {
+    this.users = this.users.filter((user) => user.id !== id);
+  }
+}
