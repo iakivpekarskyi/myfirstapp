9265535a75e23f0f48767619fcfdd9ec0466dcd1
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2024-03-14 17:12:00 +0100

    feat: create user-card component

--- src/app/components/user-card/user-card.component.ts
+++ src/app/components/user-card/user-card.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from '../../types/users.interface';
 
 @Component({
   selector: 'app-user-card',
   standalone: true,
   imports: [],
   templateUrl: './user-card.component.html',
-  styleUrl: './user-card.component.scss'
+  styleUrl: './user-card.component.scss',
 })
 export class UserCardComponent {
+  @Input() user: User;
 
+  constructor() {
+    this.user = {} as User;
+  }
 }
--- src/app/components/users-list/users-list.component.ts
+++ src/app/components/users-list/users-list.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { UsersApiService } from '../../services/users-api.service';
 import { User } from '../../types/users.interface';
+import { UserCardComponent } from '../user-card/user-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-users-list',
   standalone: true,
-  imports: [],
+  imports: [UserCardComponent, CommonModule],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss',
   providers: [UsersApiService],
